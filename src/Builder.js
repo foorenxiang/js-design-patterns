fdb8f110c553480a2767eb02681036d3ce651820
Ren Xiang <foorenxiang@gmail.com>
2021-09-20 00:01:01 +0800

    fix builder pattern

--- src/Builder.js
+++ src/Builder.js
@@ -1,8 +1,8 @@
 class Builder {
-  product = () => {};
-  produce_part_a = () => {};
-  produce_part_b = () => {};
-  produce_part_c = () => {};
+  product() {}
+  produce_part_a() {}
+  produce_part_b() {}
+  produce_part_c() {}
 }
 
 class ConcreteBuilder1 extends Builder {
@@ -11,33 +11,26 @@ class ConcreteBuilder1 extends Builder {
     this.reset();
   }
 
-  #product;
-
   reset() {
-    console.log("Creating new product for #product");
-    this.#product = new Product1();
-    console.log("Finished creating new product for #product");
+    this._product = new Product1();
   }
 
   get product() {
-    const product = { ...this.#product };
+    const product = this._product;
     this.reset();
-    console.log("Product:");
-    console.log(product);
-    return "I am product";
     return product;
   }
 
   produce_part_a() {
-    this.#product.add("PartA1");
+    this._product.add('PartA1');
   }
 
   produce_part_b() {
-    this.#product.add("PartB1");
+    this._product.add('PartB1');
   }
 
   produce_part_c() {
-    this.#product.add("PartC1");
+    this._product.add('PartC1');
   }
 }
 
@@ -51,19 +44,56 @@ class Product1 {
   }
 
   list_parts() {
-    console.log(this.parts);
+    console.log(`Product parts: ${this.parts.join(', ')}`);
   }
 }
 
+class Director {
+  constructor() {
+    this._builder = null;
+  }
+
+  get builder() {
+    return this._builder;
+  }
+
+  set builder(builder) {
+    this._builder = builder;
+  }
+
+  build_minimal_viable_product() {
+    this.builder.produce_part_a();
+  }
+
+  build_full_featured_product() {
+    this.builder.produce_part_a();
+    this.builder.produce_part_b();
+    this.builder.produce_part_c();
+  }
+}
+
+const client_code = () => {};
+
 const main = () => {
+  client_code();
+  const director = new Director();
   const builder = new ConcreteBuilder1();
-  console.log("Custom product: ");
+  director.builder = builder;
+
+  console.log('Standard basic product: ');
+  director.build_minimal_viable_product();
+  builder.product.list_parts();
+
+  console.log('');
+
+  console.log('Standard full featured product: ');
+  director.build_full_featured_product();
+  builder.product.list_parts();
+
+  console.log('Custom product:');
   builder.produce_part_a();
   builder.produce_part_b();
-  console.log("Printouts");
-  console.log(builder.product);
-  console.log(builder.product());
-  console.log(builder.product.list_parts);
+  builder.product.list_parts();
 };
 
 main();
